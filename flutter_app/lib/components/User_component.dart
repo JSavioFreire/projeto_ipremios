c5f672d8e4828c77f548175d9e69ccd97607d5d5
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2022-12-09 11:59:40 -0300

    do

---
+++ flutter_app/lib/components/User_component.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class UserComponents extends StatefulWidget {
+  const UserComponents({super.key});
+
+  @override
+  State<UserComponents> createState() => _UserComponentsState();
+}
+
+class _UserComponentsState extends State<UserComponents> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ flutter_app/lib/screens/home_page.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+
+
+//Future fetch() async {
+//  var url = Uri.parse('https://reqres.in/api/users?page=1');
+//  var response = await http.get(url);
+//  var json = jsonDecode(response.body);
+//  return json['data'];
+//}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista desafio IPREMIOS'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          for (var i = 0; i < 6; i++)
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: UserComponent(),
+            )
+        ],
+      ),
+    );
+  }
+}
