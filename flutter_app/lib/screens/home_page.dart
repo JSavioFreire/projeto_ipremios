17a91786f442dbee9f670187dc9cd05180856085
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2022-12-12 16:42:44 -0300

    quase

--- flutter_app/lib/screens/home_page.dart
+++ flutter_app/lib/screens/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import '../components/user_component.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,18 +11,24 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-
-
-//Future fetch() async {
-//  var url = Uri.parse('https://reqres.in/api/users?page=1');
-//  var response = await http.get(url);
-//  var json = jsonDecode(response.body);
-//  return json['data'];
-//}
+callApi() async {
+  var client = http.Client();
+  try {
+    var response =
+        await client.get(Uri.parse('https://reqres.in/api/users?page=1'));
+    var res = jsonDecode(response.body);
+    var resData = res['data'] as List;
+    List<ObApi> obs = resData.map((e) => ObApi.fromJson(e)).toList();
+    print(obs);
+  } finally {
+    client.close();
+  }
+}
 
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    callApi();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lista desafio IPREMIOS'),
@@ -35,5 +44,26 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+}
+
+class ObApi {
+  int? id;
+  String? email;
+  String? firstName;
+  String? lastName;
+  String? avatar;
+
+  ObApi({this.id, this.email, this.firstName, this.lastName, this.avatar});
+
+  ObApi.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    email = json['email'];
+    firstName = json['first_name'];
+    lastName = json['last_name'];
+    avatar = json['avatar'];
+  }
+  String toString() {
+    return 'id : $email';
   }
 }
